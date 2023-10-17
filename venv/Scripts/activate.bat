26ddc658596a8caca25eefbd135eeeb67b068fc4
MATHEUS SUARTE TAVARES PASSOS <matheus.suarte@a.catolica-to.edu.br>
2023-10-17 20:44:38 -0300

    Criação de outros arquivos html e configuração das views

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\INTEL\Documents\Projeto django\Djangogirls\venv
+set VIRTUAL_ENV=C:\Users\Matheus.suarte\Documents\Djangogirls\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
